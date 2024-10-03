a8edf00d60970c49138a2306d6f03af19be64d14
Berko-Adepa-Agnes <a.berko1@alustudent.com>
2024-10-03 21:10:21 +0000

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-from fabric import Connection
+"""Fabric script which generates a tgz archive"""
+
 from datetime import datetime
-import os
+from fabric.api import local
+from os.path import isdir
+
 
 def do_pack():
-    """
-    Generates a .tgz file from the contents of the web_static folder.
-
-    Returns:
-        str: The file path of the generated .tgz file if successful, else None.
-    """
-    
-    date_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_name = "versions/web_static_{}.tgz".format(date_time)
-    
+    """TGZ"""
     try:
-        os.makedirs("versions", exist_ok=True)  # Create versions directory if it doesn't exist
-        # Use Connection to execute local commands
-        conn = Connection("localhost")  # Using local machine for packing
-        
-        conn.run("tar -zcvf {} web_static".format(file_name))
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except Exception as err:
-        print(f"Error occurred: {err}")
+    except BaseException:
         return None
